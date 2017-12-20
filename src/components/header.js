c765af8d8413146325b4a1593ae7dc02a2dedf27
Phani Ganti <phani@soy.idg.com.au>
2017-12-20 16:23:27 +1100

    Added the header component for movie item and added styles to movie.js

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Header = () => {
+  return (
+      <header>
+          <Link to="/">MOVIE DB</Link>
+      </header>
+  )
+}
+
+export default Header
--- src/components/movie.js
+++ src/components/movie.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
+import Header from './header'
+
+const REQ_MOVIE_DETAIL = `http://www.omdbapi.com/?i=`
+const API_KEY = 'fc4557f7'
 
 class Movie extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch(`${REQ_MOVIE_DETAIL}${this.props.match.params.movieid}&apikey=${API_KEY}`, {
+        method:'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        movie: json
+      })
+    })
+  }
+
   render(){
     return(
-        <div>Movie</div>
+      <div>
+        <Header/>
+        <div className="movie_bio">
+            <div className="avatar">
+                <span style={{background:`url('${this.state.movie.Poster}') no-repeat`}}></span>
+            </div>
+            <div className="bio">
+                <h3>{this.state.movie.Title}</h3>
+                <div className="bio_text">
+                    {this.state.movie.Plot}
+                </div>
+
+            </div>
+        </div>
+      </div>
     )
   }
 }
