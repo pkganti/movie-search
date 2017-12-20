de14356c69bb6e9c4a826a32737ea0822868609e
Phani Ganti <phani@soy.idg.com.au>
2017-12-20 14:20:26 +1100

    Added the API url and styling for the banner image and the movie list

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
 
 import Banner from '../components/banner'
+import MovieList from '../components/movielist'
+const URL_MOVIE_API = 'http://www.omdbapi.com/?s=jaws&apikey=fc4557f7'
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch(URL_MOVIE_API, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        movies: json['Search']
+      })
+    })
+  }
 
   render(){
     return(
         
         <div>
             <Banner></Banner>
+            <MovieList allMovies={this.state.movies}></MovieList>
         </div>
     )
   }
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react'
+import React  from 'react'
+import { Link } from 'react-router-dom'
 
-class MovieList extends Component {
+const MovieList = (props) => {
 
-  render(){
-    return(
-        <div>MovieList</div>
-    )
+  const list = ({allMovies}) =>{
+    if(allMovies){
+      console.log(allMovies)
+      return allMovies.map((item) =>{
+        const style = {
+          background: `url('${item.Poster}') no-repeat`
+        }
+        return(
+            <Link key={item.imdbID} to={`/movie/${item.imdbID}`}
+                className="movie_item"
+                style={style}
+            >
+                <div>{item.Title}</div>
+                
+            </Link>
+        )
+      })
+    }
   }
+  return(
+      <div className='movie_list'>
+          <h4>Browse the movies</h4>
+          {list(props)}
+      </div>
+  ) 
 }
 
 export default MovieList
